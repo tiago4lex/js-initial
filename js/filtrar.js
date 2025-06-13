39d27821b3a347e3096e3d569562505908bb2f7b
Tiago <tiago.4lexandre@gmail.com>
2025-06-12 21:30:58 -0300

    Adiciona funcionalidade de filtro para pacientes

---
+++ js/filtrar.js
@@ -0,0 +1,21 @@
+var campofiltro = document.querySelector("#filtrar-tabela");
+
+campofiltro.addEventListener("input", function () {
+  var pacientes = document.querySelectorAll(".paciente");
+
+  if (this.value.length > 0) {
+    for (let i = 0; i < pacientes.length; i++) {
+      const paciente = pacientes[i];
+      var tdNome = paciente.querySelector(".info-nome").textContent;
+
+      // Regex ou Expressão regular para ignorar acentos
+      var expressao = new RegExp(this.value, "i");
+
+      if (!expressao.test(tdNome)) {
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    }
+  }
+});
